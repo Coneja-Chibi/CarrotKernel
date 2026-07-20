c736e771449562be5a9c842315d65dabf9a92f6f
Coneja-Chibi <chibiconeja@gmail.com>
2026-07-19 23:36:23 -0400

    Fix character repository detail lookup

--- repository-manager.js
+++ repository-manager.js
@@ -348,7 +348,7 @@ function navigateToRepository(repoName) {
 
 function navigateToCharacter(characterName, repoName) {
     setCurrentRepoView('character');
-    setSelectedCharacter(characterName);
+    setSelectedCharacter(`${repoName}::${characterName}`);
     setSelectedRepository(repoName);
     renderRepositoryManager();
 }
